edd1dc8f7217506d3dde93d27545bca4ac02dfbe
aaguirrebena <aaguirrebena@uc.cl>
2020-11-21 05:02:39 -0300

    mainPage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import image from "./assets/mainImageWBG.png";
+import "./App.css";
+import Cards from "./components/Cards/Cards";
 
 function App() {
+  const object = [1, 2, 3, 4, 5, 6, 7, 8];
+  const [process, setProcess] = useState("General");
+
+  function handleProcess(e, element) {
+    e.preventDefault();
+
+    setProcess(element);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__container">
+        <div className="app__header">
+          <h1> Refrigeración líquida </h1>
+        </div>
+
+        <div className="app__process">
+          <div className="app__process--title">Procesos</div>
+          <div className="app__process--option">
+            {object.map(element => (
+              <button
+                onClick={e => handleProcess(e, element)}
+                className="app__process--button"
+              >
+                {element}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <div className="app__body">
+          <div className="app__left">
+            <img
+              onClick={e => handleProcess(e, "General")}
+              src={image}
+              alt="image"
+            />
+          </div>
+          <div className="app__right">
+            <div className="app__right--title">
+              <h3>PROCESO</h3>
+            </div>
+            <div className="app__right--equations">
+              ECUACIONES RELACIONADAS AL PROCESO {process}
+              {/* <Cards number={process} /> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="app__footer">INFORMACIÓN GENERAL</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Cards({ number }) {
+  return <div>{number}</div>;
+}
+
+export default Cards;
